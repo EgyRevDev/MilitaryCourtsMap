f71fbff4ac4a5a3ecf87d85b224ab43d6d8d6d7c
Wael-Showair <showairovic@gmail.com>
2014-06-25 00:39:48 -0400

    Add a button to search for countries

--- border.js
+++ border.js
@@ -1,17 +1,21 @@
 /**
  * 
  */
+
 var world_map;
+
+var fusionTableLayer;
+
+/* I have obtained world countries borders and merged it into my own
+ * Military Country Map Fusion Table. This is the ID of my table. */
+var fusionTableID = "1Ghp2CN2txWCxVMNEBr9Q-mufpstJ68dNRGG_qLGR";
+
+/* Name of the column in Military Country Map Fusion Table. This column
+ * contains KML polygons of all world countries. Note that for performance 
+ * issues, most countries' borders are not precise. */
+var borderColumnName = "Border";
+
 function initialize() {
-
-	/* I have obtained world countries borders and merged it into my own
-	 * Military Country Map Fusion Table. This is the ID of my table. */
-	var fusionTableID = '1Ghp2CN2txWCxVMNEBr9Q-mufpstJ68dNRGG_qLGR';
-
-	/* Name of the column in Military Country Map Fusion Table. This column
-	 * contains KML polygons of all world countries. Note that for performance 
-	 * issues, most countries' borders are not precise. */
-	var borderColumnName = 'Border';
 
 	/* All countries whose military status are permitted by constitution are colored by red*/
 	var colorOfMilTrialsPermByConst = '#FF0000';
@@ -23,10 +27,10 @@ function initialize() {
 	var colorOfMilTrialsBanByLaw = '#FF9900';
 	/* All countries whose military status are banned by constitution are colored by grey*/
 	var colorOfMilTrialsUnknown = '#999999';
-	
+
 	var initial_center = new google.maps.LatLng(0, 0);
 	var intial_zoom_level = 2;
-	
+
 	/* Initial Map properties */
 	var mapOptions = {
 			center 	 : initial_center,
@@ -40,8 +44,9 @@ function initialize() {
 
 	/* For all world countries, get their borders from the fusion table through fusion table query.
 	 * Styling all world countries accordingly.*/
-	var fusionTableLayer = new google.maps.FusionTablesLayer({
+	fusionTableLayer = new google.maps.FusionTablesLayer({
 		query: {
+			/* Make sure to represent given string of different query fields with the same symbol either double quotation "" or single quotation ''.*/
 			select: borderColumnName,
 			from: fusionTableID
 		},
@@ -75,9 +80,34 @@ function initialize() {
 			}
 		}]
 	});
-	
+
 	/* Draw fusion table layer over given map. */
 	fusionTableLayer.setMap(world_map);
+}
+
+function showSelectedCountry() {
+
+	console.log("Inside highlightCountry function");
+	var whereClause;
+	var searchString = document.getElementById('search-string_0').value.replace(/'/g, "\\'");
+	console.log("searchString var is: %s", searchString);
+
+	if (searchString != '--Select--') {
+		whereClause = "Name CONTAINS IGNORING CASE \'"+ searchString+"\'";
+		console.log("whereClause var is: %s", whereClause);
+	}
+
+	fusionTableLayer.setOptions({
+		query: {
+			/* Make sure to represent given string of different query fields with the same symbol either double quotation "" or single quotation ''.
+			   Here double quotation is used since there is whereClause variable is to be formed at runtime .*/
+			select: borderColumnName,
+			from  : fusionTableID,
+			where : whereClause
+		}
+
+	});
+
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
