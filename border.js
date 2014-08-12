ad94418ac7479e0bec551ef21717d2fd64f3536a
Wael-Showair <showairovic@gmail.com>
2014-08-12 12:40:54 -0400

    add world countries polygons and color them based on military status field. Also adjust indentation of files

--- border.js
+++ border.js
@@ -4,7 +4,6 @@
 
 var world_map;
 
-
 function getDefaultMapOptions(){
 
 	var initial_center = new L.latLng(0, 0);
@@ -12,8 +11,8 @@ function getDefaultMapOptions(){
 
 	/* Initial Map properties */
 	var mapOptions = {
-            zoom: intial_zoom_level,
-            center: initial_center
+			zoom: intial_zoom_level,
+			center: initial_center
 	};
 
 	return mapOptions;
@@ -22,38 +21,175 @@ function getDefaultMapOptions(){
 function getTileOptions(){
 
 	var copyright = 'Imagery &copy; <a href="http://mapbox.com">Mapbox</a> , &copy; <a href="www.nomiltrials.com">للا للمحاكمات العسكرية للمدنيين</a>';
-	
+
 	var tileOptions= {
-	    attribution: copyright,
-	    maxZoom: 18
+			attribution: copyright,
+			minZoom: 2
 	};
-	
+
 	return tileOptions;
 }
 
-function addTile(){
-	var map_id='wshowair.j04mg6fm';
+function addTileLayer(){
+	var map_id='wshowair.xu7bvs4i';
 	var tile_url = 'https://{s}.tiles.mapbox.com/v3/' + 
-					map_id +
-					'/{z}/{x}/{y}.png';
+	map_id +
+	'/{z}/{x}/{y}.png';
 
 	var tileOptions = getTileOptions();
-	L.tileLayer(tile_url, tileOptions).addTo(world_map);
+	var tileLayer = L.tileLayer(tile_url, tileOptions);
+
+	tileLayer.addTo(world_map);
+	tileLayer.on('load', 
+			function() {
+		console.log("all visible tiles have been loaded");
+	});
 }
 
-function getDefaultQuery(){
-
-
-	return null;
+function addGeoJsonLayer(){
+	
+	/* Features in GeoJSON contain a geometry object and additional properties.
+	 * Add vector data to map */
+	geojsonLayer = L.geoJson(worldborders, {
+		style: getBaseStyle,
+		onEachFeature: setEvents
+	}).addTo(world_map);
 }
+
+
+
+
+
+function getBaseColor(milstatus){
+	var geoJsonGeometryColor;
+
+	/*console.log("switch on feature.properties.milstatus");*/
+	switch (milstatus) {
+
+	case 'PermittedByConst':
+		/*console.log("Case PermittedByConst");*/
+		geoJsonGeometryColor = "#ed1c24"; /*red*/
+		break;
+	case 'PermittedByLaw':
+		/*console.log("Case PermittedByLaw");*/
+		geoJsonGeometryColor = "#d8d82b"; /*yellow: bdc40a */
+		break;
+	case 'BannedByLaw':
+		/*console.log("Case BannedByLaw");*/
+		geoJsonGeometryColor = "#d88c00"; /* orange c68802 or c96d00 or fb8800*/
+		break;
+	case 'BannedByConst':
+		/*console.log("Case BannedByConst");*/
+		geoJsonGeometryColor = "#269326"; /* green: 136003 or 519943*/
+		break;
+	case 'Unkwon':
+		/*console.log("Case Unkwon");*/
+		geoJsonGeometryColor = "#808080";
+		break;
+	default:
+		/*console.log("Case default");*/
+		geoJsonGeometryColor = "#0000A0";
+	}
+	
+	return geoJsonGeometryColor;
+}
+
+/* Set base style of GeoJson vector data as a function of current Military Courts to Civilians state.
+ * Note that Features in GeoJSON contain a geometry object and additional properties.*/
+function getBaseStyle(feature) {
+
+	return {
+		color: "#000000", 	/* Stroke color */
+		weight: 0.2, 		/* Stroke width in pixels. */
+		opacity: 1, 		/* Stroke opacity.*/
+		fillOpacity: 0.6,   /* Fill color of geometry. */
+		fillColor: getBaseColor(feature.properties.milstatus) /* Fill opacity of geometry. */
+	};
+}
+
+function getHighlightColor(milstatus){
+	var geoJsonGeometryColor;
+
+	/*console.log("switch on feature.properties.milstatus");*/
+	switch (milstatus) {
+
+	case 'PermittedByConst':
+		/*console.log("Case PermittedByConst");*/
+		geoJsonGeometryColor = "#ff0000"; /*red*/
+		break;
+	case 'PermittedByLaw':
+		/*console.log("Case PermittedByLaw");*/
+		geoJsonGeometryColor = "#FFFF00"; /*yellow */
+		break;
+	case 'BannedByLaw':
+		/*console.log("Case BannedByLaw");*/
+		geoJsonGeometryColor = "#ffa500"; /* orange */
+		break;
+	case 'BannedByConst':
+		/*console.log("Case BannedByConst");*/
+		geoJsonGeometryColor = "#00FF00"; /* green */
+		break;
+	case 'Unkwon':
+		/*console.log("Case Unkwon");*/
+		geoJsonGeometryColor = "#C0C0C0";
+		break;
+	default:
+		/*console.log("Case default");*/
+		geoJsonGeometryColor = "#0000FF";
+	}
+	
+	return geoJsonGeometryColor;
+}
+
+/* Set base hover of GeoJson vector data as a function of current Military Courts to Civilians state.
+ * Note that Features in GeoJSON contain a geometry object and additional properties.*/
+function highlightFeature(e) {
+
+	/*var countryName = e.target.feature.properties.name;
+	console.log(countryName);*/
+	var layer = e.target;
+	layer.setStyle({
+		color: '#FFFFFF',
+		weight: 1,
+		opacity: 1,
+		/*dashArray: '3',*/
+		fillOpacity: 0.7,
+		fillColor: getHighlightColor(e.target.feature.properties.milstatus)
+	});
+	
+	if (!L.Browser.ie && !L.Browser.opera) {
+        layer.bringToFront();
+    }
+}
+
+/* A function to reset the colors when a country is not longer 'hovered' */
+function resetHighlight(e) {
+	geojsonLayer.resetStyle(e.target);
+}
+
+
+
+/* Tell Leaflet what functions to call when mousing over and out of a country */
+function setEvents(feature, layer) {
+	layer.on({
+		mouseover	: highlightFeature,
+		mouseout	: resetHighlight
+	});
+}
+
+
 function initialize() {
 
 	var mapOptions = getDefaultMapOptions();
 
 	/* create a JavaScript "map" object, passing it the div element named "map-canvas" and the map properties. */
-	world_map = new L.map("map-canvas",mapOptions);
+	world_map = new L.map("map-canvas",mapOptions)
+	/*.addControl(L.mapbox.shareControl())*/;
+
+	addTileLayer();
+
+	addGeoJsonLayer();
 	
-	addTile();
 	
 	/* In case Enter key is pressed, zoom to the selected country (if any)*/
 	/*google.maps.event.addDomListener(window, 'keypress',function(e) {
